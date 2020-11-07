6f08dd3a04b9ad60f30770a53ca3cb2c2f89a8d1
Adams Anaglo <adamsanaglo@gmail.com>
2020-11-07 03:15:45 +0000

    Create script.py

---
+++ script.py
@@ -0,0 +1,35 @@
+import nltk
+import re
+import string
+from nltk.tokenize import word_tokenize, sent_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer # or LancasterStemmer, RegexpStemmer, SnowballStemmer
+
+default_stemmer = PorterStemmer()
+default_stopwords = stopwords.words('english') # or any other list of your choice
+def clean_text(text, ):
+
+    def tokenize_text(text):
+        return [w for s in sent_tokenize(text) for w in word_tokenize(s)]
+
+    def remove_special_characters(text, characters=string.punctuation.replace('-', '')):
+        tokens = tokenize_text(text)
+        pattern = re.compile('[{}]'.format(re.escape(characters)))
+        return ' '.join(filter(None, [pattern.sub('', t) for t in tokens]))
+
+    def stem_text(text, stemmer=default_stemmer):
+        tokens = tokenize_text(text)
+        return ' '.join([stemmer.stem(t) for t in tokens])
+
+    def remove_stopwords(text, stop_words=default_stopwords):
+        tokens = [w for w in tokenize_text(text) if w not in stop_words]
+        return ' '.join(tokens)
+
+    text = text.strip(' ') # strip whitespaces
+    text = text.lower() # lowercase
+    text = stem_text(text) # stemming
+    text = remove_special_characters(text) # remove punctuation and symbols
+    text = remove_stopwords(text) # remove stopwords
+    #text.strip(' ') # strip whitespaces again?
+
+    return text
